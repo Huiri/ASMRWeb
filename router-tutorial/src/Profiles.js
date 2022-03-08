536d0c13558533266ec07d776427aedbe473e1d1
Huiri <gmlfltj@naver.com>
2022-03-08 21:28:26 +0900

    라우터 부가기능(history, withRouter, switchm navLink)

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
+import WithRouterSample from './WithRouterSample';
 
 const Profiles = () => {
+    const activeStyle = {
+        background : 'black',
+        color : 'white'
+    };
     return (
         <div>
             <h3>사용자 목록 : </h3>
             <ul>
                 <li>
-                    <Link to="/profiles/chaz">chaz</Link>
+                    <NavLink activeStyle = {activeStyle} to="/profiles/chaz">chaz</NavLink>
                 </li>
                 <li>
-                    <Link to="/profiles/happy">happy</Link>
+                    <NavLink activeStyle = {activeStyle} to="/profiles/happy">happy</NavLink>
                 </li>
             </ul>
 
@@ -21,6 +26,7 @@ const Profiles = () => {
             render={()=><div>사용자를 선택해주세요</div>}
             />
             <Route path="/profiles/:username" component={Profile}/>
+            <WithRouterSample/>
         </div>
     );
 };
